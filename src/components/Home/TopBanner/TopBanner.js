28eb99131c76a6395601c0d7dfba2fa9889d7802
sakib1416 <sakib1416@gmail.com>
2021-04-30 15:56:50 +0200

    update checking

--- src/components/Home/TopBanner/TopBanner.js
+++ src/components/Home/TopBanner/TopBanner.js
@@ -5,7 +5,7 @@ const TopBanner = () => {
     return (
         <div className="top-banner-container">
             <div className="text-center top-banner-main">
-                <h1 className="">Explore the world with <br/> Travel Venture</h1>
+                <h1 className="">Explore the greater world with <br/> Travel Venture</h1>
                 <button type="button" class="btn btn-secondary btn-lg">Let's Explore</button>
             </div>
         </div>
